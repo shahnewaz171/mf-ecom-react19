cfed2d9c07c35f1262e448f0f853c33727a013ff
shahnewaz171 <shahnewaz601@gmail.com>
2025-10-31 22:35:14 +0600

    fix: fixed remote entry issue

--- apps/products/module-federation.config.ts
+++ apps/products/module-federation.config.ts
@@ -2,6 +2,7 @@ import { createModuleFederationConfig } from '@module-federation/enhanced/rspack
 
 export default createModuleFederationConfig({
   name: 'products',
+  filename: 'remoteEntry.js',
   exposes: {
     './ProductsGrid': './src/components/products/products-grid.tsx',
     './useTest': './src/components/hooks/useTest.ts',
